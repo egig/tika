12dd515d1808d2d22b922f098037f759672d8441
egi <egigundari@gmail.com>
2014-10-09 09:04:11 +0700

    add travis and fixes indentation

--- src/Tika.php
+++ src/Tika.php
@@ -4,107 +4,107 @@ use Carbon\Carbon;
 
 class Tika extends Carbon {
 
-    /**
-     * @var string
-     */
-    protected $locale;
+	/**
+	 * @var string
+	 */
+	protected $locale;
 
 	/**
 	 * @var array
 	 */
 	protected $languagePack = array();
 
-    /**
-     * @var array
-     */
+	/**
+	 * @var array
+	 */
 	protected $languageFile = array();
 
-    /**
-     * @var array
-     */
-    protected static $builtInLanguages = array('id');
+	/**
+	 * @var array
+	 */
+	protected static $builtInLanguages = array('id');
 
 	/**
 	 * @{inheritdoc}
 	 */
 	public function __construct($time = null, $tz = null, $locale = null)
 	{
-        date_default_timezone_set($tz);
+		date_default_timezone_set($tz);
 
 		parent::__construct($time, $tz);
 
-        $this->locale = is_null($locale) ? 'id' : $locale;
+		$this->locale = is_null($locale) ? 'id' : $locale;
 
-        foreach (static::$builtInLanguages as $lang) {
+		foreach (static::$builtInLanguages as $lang) {
 		  $this->setLanguageFilePath($lang, __DIR__ .'/l10n/'.$lang.'.php');
-        }
+		}
 	}
 
-    /**
-     * @{inheritdoc}
-     */
-    public function format($format)
-    {
-         return $this->translate(parent::format($format));
-    }
+	/**
+	 * @{inheritdoc}
+	 */
+	public function format($format)
+	{
+		return $this->translate(parent::format($format));
+	}
 
-    /**
-     * Translate given date
-     *
-     * @param string $data
-     */
-    protected function translate($date)
-    {
-    	$replaces = $this->getLanguagePack($this->locale);
+	/**
+	 * Translate given date
+	 *
+	 * @param string $data
+	 */
+	protected function translate($date)
+	{
+		$replaces = $this->getLanguagePack($this->locale);
 
-    	return strtr($date, $replaces);
-    }
+		return strtr($date, $replaces);
+	}
 
-    /**
-     * set locale
-     *
-     * @param string $locale
-     */
-    public function setlocale($locale)
-    {
-        $this->locale = $locale;
-    }
+	/**
+	 * set locale
+	 *
+	 * @param string $locale
+	 */
+	public function setlocale($locale)
+	{
+		$this->locale = $locale;
+	}
 
-    /**
-     * Get language pack
-     *
-     * @param
-     */
-    private function getLanguagePack($locale)
-    {
-    	if(! isset($this->languagePack[$locale])) {
-    		$this->languagePack[$locale] = file_exists($this->getLanguageFilePath($locale)) ?
-                require $this->getLanguageFilePath($locale) : array();
-    	}
+	/**
+	 * Get language pack
+	 *
+	 * @param
+	 */
+	private function getLanguagePack($locale)
+	{
+		if(! isset($this->languagePack[$locale])) {
+			$this->languagePack[$locale] = file_exists($this->getLanguageFilePath($locale)) ?
+				require $this->getLanguageFilePath($locale) : array();
+		}
 
-    	return $this->languagePack[$locale];
-    }
+		return $this->languagePack[$locale];
+	}
 
-    /**
-     * set language file path
-     *
-     * @param string $locale
-     * @param string $path
-     */
-    public function setLanguageFilePath($locale, $path)
-    {
-    	$this->languageFile[$locale] = $path;
-    }
+	/**
+	 * set language file path
+	 *
+	 * @param string $locale
+	 * @param string $path
+	 */
+	public function setLanguageFilePath($locale, $path)
+	{
+		$this->languageFile[$locale] = $path;
+	}
 
 
-    /**
-     * Get language file path
-     *
-     * @param string $locale
-     * @return string
-     */
-    public function getLanguageFilePath($locale)
-    {
-    	return isset($this->languageFile[$locale])  ? $this->languageFile[$locale] : false;
-    }
+	/**
+	 * Get language file path
+	 *
+	 * @param string $locale
+	 * @return string
+	 */
+	public function getLanguageFilePath($locale)
+	{
+		return isset($this->languageFile[$locale])  ? $this->languageFile[$locale] : false;
+	}
 }
